2eab15af28de1b81778d30c87d5154b3e858cdac
Salvatore Laiso <salvatore.laiso@it.ey.com>
2024-02-01 11:14:23 +0100

    fix: update Satosa base to Alpine 3.19.1

--- example/entrypoint.sh
+++ example/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. /.venv/bin/activate
+
 # get IDEM MDQ key
 if [[ $GET_IDEM_MDQ_KEY == true ]]; then
   wget https://mdx.idem.garr.it/idem-mdx-service-crt.pem -O $KEYS_FOLDER/idem-mdx-service-crt.pem
